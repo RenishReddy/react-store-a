eb53b72d1b048e68a4235a822175cdd97fa63b8e
RenishReddy <alavalarenish@gmail.com>
2025-04-01 13:48:32 +0530

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,18 +2,19 @@ import "./Products.css";
 import { appContext } from "../App";
 import { useContext } from "react";
 export default function Products() {
-  const { user, products } = useContext(appContext);
-
+  const { user, products, cart, setCart } = useContext(appContext);
+  const addToCart = (id) => {
+    setCart({ ...cart, [id]: 1 });
+  };
   return (
     <div>
       {user.name}
       <div className="App-Products-Row">
-        
         {products.map((value, index) => (
           <div className="App-Products-Box" key={index}>
             <h3>{value.name}</h3>
             <h4>{value.price}</h4>
-            <button>Add to Cart</button>
+            <button onClick={() => addToCart(value.id)}>Add to Cart</button>
           </div>
         ))}
       </div>
