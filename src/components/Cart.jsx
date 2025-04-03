7eb166687dfc35df2fcee02ee7a8f031d22ea77a
RenishReddy <alavalarenish@gmail.com>
2025-04-03 09:06:00 +0530

    tttt

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,13 +6,14 @@ export default function Cart() {
   const handleDelete = (id) => {
     setCart({ ...cart, [id]: 0 });
   };
+  
   return (
     <div>
       {products.map(
         (value) =>
           cart[value.id]>0 && (
             <div key={value.id}>
-              {value.name}-{value.price}-{cart[value.id]}-
+              {value.name}-{value.price}-<button>-</button>{cart[value.id]}<button>+</button>-
               {value.price * cart[value.id]}-
               <button onClick={() => handleDelete(value.id)}>Delete</button>
             </div>
